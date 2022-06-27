56785efaf27b41756a67050a769563595df40c74
anniep8911 <pjatopazz@gmail.com>
2022-06-27 18:57:11 +0900

    ver02

--- onon/js/mob_common.js
+++ onon/js/mob_common.js
@@ -2,7 +2,6 @@ $(function () {
     //변수
     var $totop = $('.ftrWrap footer'),
         $portWrap = $('.portfolio .artGroup'),
-        $htmlWidth = $('html').width(),
         site=[],
         $carr = $('.careers section article'),
         $sks = $('.skills section article div');
@@ -11,13 +10,13 @@ $(function () {
         
 
         $.getJSON('data/site.json',data).fail(function(){console.log('fail')});
-        
             function data(d){
                 for(i=0;i<=d.href.length-1;i++){
                     site[i]=d.href[i];
-                    $portWrap.find('article').eq(i+1).find('a').attr('href',site[i]);
+                    $portWrap.find('article').eq(i).find('a').attr('href',site[i]);
                 }
-            }
+        }
+        console.log(site);
            
         $carr.on('click',careers);
         $sks.on('click',skills);
@@ -37,17 +36,18 @@ $(function () {
     function top(){
         $('html').animate({scrollTop:0}, 1000);
     }
+    
 
     //모바일 태블릿 구분
-    if ($htmlWidth <= 1024) {
         //전처리
-        $portWrap.css({marginLeft: '-100%'});
-        $portWrap.find('article:last').prependTo($portWrap);
+        setTimeout(function(){
+            $portWrap.find('article:last').prependTo($portWrap);
+        },50);
 
         //이벤트
         $totop.on('click', top);
         $portWrap.on('swipeleft swiperight', swipe);
-        
+        $portWrap.css({marginLeft:'-100%'});
         //함수
         function swipe(e) {
             switch (e.type) {
@@ -70,39 +70,5 @@ $(function () {
                 });
             }
         }
-
-    } else {
-        //전처리
-        $portWrap.css({marginLeft: '-45%'});
-        $portWrap.find('article:last').prependTo($portWrap);
-
-        //이벤트
-        $totop.on('click', top);
-        $portWrap.on('swipeleft swiperight', swipe);
-
-        //함수
-        function swipe(e) {
-            switch (e.type) {
-                case "swipeleft":swLeft();
-                break;
-                case "swiperight":swRight();
-                break;
-                default:console.log('나올수없는 값');
-            }
-            function swLeft() {
-                $portWrap.stop().animate({marginLeft:'-90%'},500,'linear',function(){
-                    $portWrap.css({marginLeft:'-45%'});
-                    $portWrap.find('article:first').appendTo($portWrap);
-                });
-            }
-            function swRight() {
-                $portWrap.stop().animate({ marginLeft: '0%'},500,'linear', function(){
-                    $portWrap.css({marginLeft: '-45%'});
-                    $portWrap.find('article:last').prependTo($portWrap);
-                });
-            }
-        }
-
-    }
 
 });
